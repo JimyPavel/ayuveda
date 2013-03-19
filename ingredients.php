e4c4b07ca8c0842189c483c497431a59903f0ec6
jimypavel <jimy_jimy_16@yahoo.com>
2013-03-20 01:38:41 +0200

    update1

--- ingredients.php
+++ ingredients.php
@@ -65,174 +65,183 @@
 </head>
 	
 	<body class="metrouicss" style="text-align:center" >
-		
-		<input type='hidden' id='hiddenData' value="<?php echo $allValues ?>" />
-		<script>
-			var items = new Array();
-			var selectedItems = new Array();
-			var allDishes = new Array();
-			var text = document.getElementById("hiddenData").value;
-			var currentExpandedDish = -1;	// no dish is expanded
-			var editPanelDim = 0;
-			if (text != null && text != "") {
-				items = text.split("|");
-			}
-			
-			// autocomplete for the ingredients
-		  $(function() {
-			var availableTags = items;
-			$( "#tags" ).autocomplete({
-			  source: availableTags
-			});
-		  });
+		<div id="wrapper">
+			<input type='hidden' id='hiddenData' value="<?php echo $allValues ?>" />
+			<script>
+				var items = new Array();
+				var selectedItems = new Array();
+				var allDishes = new Array();
+				var text = document.getElementById("hiddenData").value;
+				var currentExpandedDish = -1;	// no dish is expanded
+				var editPanelDim = 0;
+				if (text != null && text != "") {
+					items = text.split("|");
+				}
+				
+				// autocomplete for the ingredients
+			  $(function() {
+				var availableTags = items;
+				$( "#tags" ).autocomplete({
+				  source: availableTags
+				});
+			  });
+			  
+			  // autocomplete for dish category
+			  var categoryItems = new Array();
+			  categoryItems[0] = "Pizza";
+			  categoryItems[1] = "Salads";
+			  categoryItems[2] = "Appetizers";
+			  categoryItems[3] = "Beverages";
+			  categoryItems[4] = "Chicken";
+			  categoryItems[5] = "Pasta";
+			  categoryItems[6] = "Seafood";
+			  categoryItems[7] = "Rib/Steaks";
+			  categoryItems[8] = "Burger/Sandwiches";
+			  categoryItems[9] = "Healty Options";
+			  categoryItems[10] = "Desserts";
+			  categoryItems[11] = "Drinks";
+			  categoryItems[12] = "Other";
+			  
+			  $(function() {
+				var availableTags = categoryItems;
+				$( "#category" ).autocomplete({
+				  source: availableTags
+				});
+			  });
+			  
+		  </script>
 		  
-		  // autocomplete for dish category
-		  var categoryItems = new Array();
-		  categoryItems[0] = "Pizza";
-		  categoryItems[1] = "Salads";
-		  categoryItems[2] = "Appetizers";
-		  categoryItems[3] = "Beverages";
-		  categoryItems[4] = "Chicken";
-		  categoryItems[5] = "Pasta";
-		  categoryItems[6] = "Seafood";
-		  categoryItems[7] = "Rib/Steaks";
-		  categoryItems[8] = "Burger/Sandwiches";
-		  categoryItems[9] = "Healty Options";
-		  categoryItems[10] = "Desserts";
-		  categoryItems[11] = "Drinks";
-		  categoryItems[12] = "Other";
-		  
-		  $(function() {
-			var availableTags = categoryItems;
-			$( "#category" ).autocomplete({
-			  source: availableTags
-			});
-		  });
-		  
-	  </script>
-	  
-		<div style="float:left ; margin-top:190px ; height:300px; margin-left:20px"
-		id="navigationMenu">
-			<p >Navigation Menu</p>
-			<div class="page-sidebar" style="margin-right:10px;width:120px">
-				<ul>
-					<li class="sticker sticker-color-orange">
-						<form id="indexForm" method="post" action="index.php">
-							<input type="hidden" name="indexData" id="indexData" />
-							<a href = "javascript:{submitForm('indexForm')}">About us</a>
-						</form>
-					</li>
-					<li class="sticker sticker-color-green" style="background-color:green">
-						<a style="color:white" >Create menu</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-		
-		<div style="float:right;margin-top:190px; width:200px;height:300px"
-		id = "optionsMenu">
-			<p >Options Menu</p>
-			<div class= "page-sidebar" style="width:150px; margin-left:25px">
-				<ul>
-					<li>
-						<a id="addDishMenu" style="visibility:collapse" 
-						href="javascript:{addDish()}"><i class="icon-box-add"></i> Add dish</a>
-						<a id="exportPdf" style="visibility:collapse" 
-						href="javascript:{exportPDF()}"><i class="icon-download"></i> Export to PDF</a>
-						<a id="deleteDish" style="visibility:collapse" 
-						href="javascript:{deleteDish()}"><i class="icon-remove"></i> Delete dish</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-		
-		<div class="page" >
-			<div class="page-header">
-				<div class="page-header-content">
-					<h1> Welcome to Ayuveda </h1>
+			<div style="float:left ; margin-top:190px ; height:300px; margin-left:20px"
+			id="navigationMenu">
+				<p >Navigation Menu</p>
+				<div class="page-sidebar" style="margin-right:10px;width:120px">
+					<ul>
+						<li class="sticker sticker-color-orange">
+							<form id="indexForm" method="post" action="index.php">
+								<input type="hidden" name="indexData" id="indexData" />
+								<a href = "javascript:{submitForm('indexForm')}">About us</a>
+							</form>
+						</li>
+						<li class="sticker sticker-color-green" style="background-color:green">
+							<a style="color:white" >Create menu</a>
+						</li>
+						<li class="sticker sticker-color-blue" >
+							<a href="team.php" >Our team</a>
+						</li>
+					</ul>
 				</div>
 			</div>
 			
-			
-			<div class="page secondary bg-color-greenDark" style="height:430px ; margin-top:50px;margin-bottom:50px">
+			<div style="float:right;margin-top:190px; width:200px;height:300px"
+			id = "optionsMenu">
+				<p style="margin-left:430px; width:100px">Options Menu</p>
+				<div class= "page-sidebar" style="width:150px;margin-left:420px">
+					<ul>
+						<li>
+							<a id="addDishMenu" style="visibility:collapse" 
+							href="javascript:{addDish()}"><i class="icon-box-add"></i> Add dish</a>
+							<a id="exportPdf" style="visibility:collapse" 
+							href="javascript:{exportPDF()}"><i class="icon-download"></i> Export to PDF</a>
+							<a id="deleteDish" style="visibility:collapse" 
+							href="javascript:{deleteDish()}"><i class="icon-remove"></i> Delete dish</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		
+			<div class="page" style="margin-left:200px">
 				<div class="page-header">
-					<div class="page-header-content" style="margin-bottom:20px">
-						<h1 class="fg-color-white">Create menu<small> with your custom dishes</small></h1>
+					<div class="page-header-content" style="width:800px">
+						<span> 
+							<h1> Welcome to</h1>
+						</span>
+						<span style="float:right;margin-top:45px;margin-right:10px">
+							<img src = "images/logo3.png" />
+						</span>
 					</div>
 				</div>
-				<div class="page-region" >
-					<div class="page-region-content" >
-						<div class="grid" >
-							<div class = "row" >
-								<div class ="span5" style="margin-left:-60px;margin-right:50px;height:430px" >
-									<div class="input-control text" style="margin-bottom:50px">
-										<input id="category" 
-										onkeypress="appear('div_dishName')" 
-										type="text" 
-										class="with-helper" placeholder="Enter Category. Eg: Pizza"/>
-										<button class="helper"></button>
-									</div>
-									<div id="div_dishName"
-									class="input-control text" 
-									style="visibility:collapse;margin-bottom:50px">
-										<input id="dishName"
-										type="text" 
-										onkeypress="appear('div_price')" 
-										class="with-helper" placeholder="Enter Dish Name. Eg: Rustic Pizza"/>
-										<button class="helper"></button>
-									</div>
-									<div id="div_price" style="visibility:collapse;margin-bottom:50px"
-									onkeypress="appear('div_ingredients')">
-										<div class="input-control text span3">
-											<input id="price" type="text" class="with-helper" placeholder="Enter price"/>
+				<div class="page secondary bg-color-greenDark" 
+				style="height:430px ; margin-top:50px;margin-bottom:50px">
+					<div class="page-header">
+						<div class="page-header-content" style="margin-bottom:20px">
+							<h1 class="fg-color-white">Create menu<small> with your custom dishes</small></h1>
+						</div>
+					</div>
+					<div class="page-region" style="position:absolute">
+						<div class="page-region-content" >
+							<div class="grid" >
+								<div class = "row" >
+									<div class ="span5" style="margin-left:-60px;margin-right:50px;height:430px" >
+										<div class="input-control text" style="margin-bottom:50px">
+											<input id="category" 
+											onkeypress="appear('div_dishName')" 
+											type="text" 
+											class="with-helper" placeholder="Enter Category. Eg: Pizza"/>
 											<button class="helper"></button>
 										</div>
-										<p> euro</p>
-									</div>
-									<div id="div_ingredients" style="visibility:collapse">
-										<div class="input-control text ui-widget">
-											<input id="tags" type="text" class="with-helper" placeholder="Enter Ingredient Name"/>
+										<div id="div_dishName"
+										class="input-control text" 
+										style="visibility:collapse;margin-bottom:50px">
+											<input id="dishName"
+											type="text" 
+											onkeypress="appear('div_price')" 
+											class="with-helper" placeholder="Enter Dish Name. Eg: Rustic Pizza"/>
 											<button class="helper"></button>
 										</div>
-										<div>
-											<button  onclick="addIngredient()">Add Ingredient</button>
+										<div id="div_price" style="visibility:collapse;margin-bottom:50px"
+										onkeypress="appear('div_ingredients')">
+											<div class="input-control text span3">
+												<input id="price" type="text" class="with-helper" placeholder="Enter price"/>
+												<button class="helper"></button>
+											</div>
+											<p> euro</p>
+										</div>
+										<div id="div_ingredients" style="visibility:collapse">
+											<div class="input-control text ui-widget">
+												<input id="tags" type="text" class="with-helper" placeholder="Enter Ingredient Name"/>
+												<button class="helper"></button>
+											</div>
+											<div>
+												<button  onclick="addIngredient()">Add Ingredient</button>
+											</div>
 										</div>
 									</div>
-								</div>
-								<div class="span5">
-									<div style="width:400px;height:420px;overflow-x:hidden;overflow-y:auto">
-										<ul 
-										class="listview" id="ingredientList" style=" margin-top:10px">
-										</ul>
+									<div class="span5">
+										<div style="width:400px;height:420px;overflow-x:hidden;overflow-y:auto">
+											<ul 
+											class="listview" id="ingredientList" style=" margin-top:10px">
+											</ul>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</div >
-			<div class="page secondary with-sidebar bg-color-blueDark"
-			name="editContent" id="editContent" style="height:300px">
-				<div class="page-header">
-					<div class="page-header-content" style="margin-bottom:20px">
-						<h1 class="fg-color-white">Edit menu</h1>
+				</div >
+				<div class="page secondary with-sidebar bg-color-blueDark"
+				name="editContent" id="editContent" style="height:300px;position:absolute">
+					<div class="page-header">
+						<div class="page-header-content" style="margin-bottom:20px">
+							<h1 class="fg-color-white">Edit menu</h1>
+						</div>
 					</div>
-				</div>
-				<div class="page-region" >
-					<div class="page-region-content" >
-						<ul class="accordion dark span10 fg-color-white" data-role="accordion" 
-						style="margin-left:-200px"
-						id="editAccordion"
-						>
-						</ul>
+					<div class="page-region" >
+						<div class="page-region-content" >
+							<ul class="accordion dark span10 fg-color-white" data-role="accordion" 
+							style="margin-left:-200px"
+							id="editAccordion"
+							>
+							</ul>
 
+						</div>
 					</div>
 				</div>
 			</div>
+			<script>
+				editPanelDim = parseInt(document.getElementById("editContent").style.height);
+			</script>
 		</div>
-		<script>
-			editPanelDim = parseInt(document.getElementById("editContent").style.height);
-		</script>
+		
 	</body>
 
 	
--- team.php
+++ team.php
@@ -1,18 +1,3 @@
-
-<?php
-	require("Data.class.php");
-	
-	// we start the session
-	session_start();
-	
-	$d = new Data();
-	$d->getContent();
-	// store session data
-	$_SESSION['data']=$d;
-	
-	// we print the first item from the data
-	//echo $d->allIngredients[0];
-?>
 <html lang="en">
 
 	<head>
@@ -20,7 +5,7 @@
 		<link href="css/modern-responsive.css" rel="stylesheet">
 		<link href="css/site.css" rel="stylesheet" type="text/css">
 		<link href="js/google-code-prettify/prettify.css" rel="stylesheet" type="text/css">
-
+		
 		<script type="text/javascript" src="js/assets/jquery-1.9.0.min.js"></script>
 		<script type="text/javascript" src="js/assets/jquery.mousewheel.min.js"></script>
 
@@ -35,27 +20,25 @@
 		<script type="text/javascript" src="js/modern/tile-slider.js"></script>
 		<script type="text/javascript" src="js/modern/tile-drag.js"></script>
 		<script type="text/javascript" src="js/modern/calendar.js"></script>
-
 	</head>
 	
 	<body class="metrouicss" style="text-align:center" >
-	
 		<div style="float:left ; margin-top:190px ; height:300px; margin-left:20px"
 		id="navigationMenu">
 			<p >Navigation Menu</p>
 			<div class="page-sidebar" style="margin-right:10px;width:120px">
 				<ul>
-					<li class="sticker sticker-color-orange" style="background-color:orange">
+					<li class="sticker sticker-color-orange" >
 						<form id="indexForm" method="post" action="index.php">
 							<input type="hidden" name="indexData" id="indexData" />
-							<a style="color:white">Why Ayuveda</a>
+							<a href="index.php">Why Ayuveda</a>
 						</form>
 					</li>
 					<li class="sticker sticker-color-green" >
 						<a href="ingredients.php" >Create menu</a>
 					</li>
-					<li class="sticker sticker-color-blue" >
-						<a href="team.php" >Our team</a>
+					<li class="sticker sticker-color-blue" style="background-color:blue">
+						<a style="color:white" href="team.php" >Our team</a>
 					</li>
 				</ul>
 			</div>
@@ -71,42 +54,51 @@
 					</span>
 				</div>
 			</div>
-			
+			<div>
+				
+			</div>
 			<div class="page secondary" style="height:430px">
 				<div class="page-header">
 					<div class="page-header-content" style="margin-bottom:20px">
-						<h1 class="fg-color-orange">Why<small>Ayuveda</small></h1>
+						<h1 class="fg-color-blue">Our<small>Team</small></h1>
 					</div>
 				</div>
-				<div class="page-region">
-					<div class="page-region-content">
-
-						<div class="span5">
-							<div class="carousel span5" style="height: 300px;width : 600px" data-role="carousel" data-param-effect="slide" data-param-direction="left" data-param-duration="2000" data-param-period="4000">
+				<div class="page-region" >
+					<div class="page-region-content" >
+						<div class="span5" >
+							<div class="carousel span5" style="height: 400px;width : 400px" 
+							data-role="carousel" data-param-effect="slide" 
+							data-param-direction="left" data-param-duration="2000" data-param-period="4000">
 								<div class="slides">
-									<div class="slide image" id="slide1">
-										<img src="images/1.jpg" />
-									</div>
 									<div class="slide image" id="slide2">
-										<img src="images/2.jpg" />
+										<img src="images/liliana.jpg" />
 									</div>
 									<div class="slide image" id="slide3">
-										<img src="images/3.jpg" />
+										<img src="images/mihai.jpg" />
+									</div>
+									<div class="slide image" id="slide4">
+										<img src="images/laur.jpg" />
+									</div>
+									<div class="slide image" id="slide1">
+										<img src="images/jimy.jpg" />
+									</div>
+									<div class="slide image" id="slide4">
+										<img src="images/razvan.jpg" />
 									</div>
 								</div>
-
-
 								<span class="control left bg-color-blue">&#8249;</span>
 								<span class="control right bg-color-blue">&#8250;</span>
-
 							</div>
+						</div>
+						<div style="margin-top:-400px;margin-left:500px ; position:absolute">
+							<p> Polytechnic University of Bucharest</p>
+							<p> <b>Romania</b></p>
+							<p> <i>Imagine Cup project 2013 </i></p>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		
 	</body>
 	
-
 </html>
